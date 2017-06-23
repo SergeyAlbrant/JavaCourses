3291feb5e9658aa8c5b5ce0d9768631a0b18b436
Сергей <@192.168.0.102>
2017-06-23 14:22:04 +0300

    First change

--- Homework170620/src/HW/HW1.java
+++ Homework170620/src/HW/HW1.java
@@ -2,6 +2,9 @@ package HW;
 
 public class HW1 {
 	
-	
+	public static void main(String[] args) {
+	      // Prints "Hello, World" in the terminal window.
+	      System.out.println("Hello, World");
+	   }
 
 }
